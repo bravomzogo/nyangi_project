607c14a371654d23b681a5fa4e960e2b49cfa816
bravolambo <bravomzogo@gmail.com>
2025-03-29 02:22:14 +0300

    shaib

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -64,22 +64,14 @@ TEMPLATES = [
     },
 ]
 
-# Database
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600
-        )
-    }
 
+# settings.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
